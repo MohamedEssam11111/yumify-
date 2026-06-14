6099d03f66dabf75f45f179b07e19a04f234198a
Mohamed Essam <mohamedessam9523@gmail.com>
2026-06-15 01:50:54 +0300

    Fix auth cookie settings for production

--- server-side/utils/tokenGen.util.js
+++ server-side/utils/tokenGen.util.js
@@ -1,5 +1,4 @@
-import jwt from 'jsonwebtoken';
-
+import jwt from "jsonwebtoken";
 
 export const generateToken = (res, user) => {
   const payload = {
@@ -10,9 +9,9 @@ export const generateToken = (res, user) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "12h" });
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 12 * 60 * 60 * 1000, 
+    secure: true,
+    sameSite: "none",
+    maxAge: 12 * 60 * 60 * 1000,
   });
 
   return token;
