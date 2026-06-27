defb5eca939625e3f6a038a526aed6b573cea782
Mohamed Essam <mohamedessam9523@gmail.com>
2026-06-27 03:40:39 +0300

    changing token security to allow http requests

--- server-side/utils/sendEmail.util.js
+++ server-side/utils/sendEmail.util.js
@@ -1,26 +1,11 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true only for port 465
+  service: "gmail",
   auth: {
     user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASS, // Google App Password
+    pass: process.env.EMAIL_PASS,
   },
-
-  connectionTimeout: 10000, // 10 sec
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-});
-
-// Verify SMTP connection when server starts
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("SMTP VERIFY ERROR:", error);
-  } else {
-    console.log("SMTP SERVER READY");
-  }
 });
 
 export const sendEmail = async (to, subject, html) => {
@@ -28,27 +13,20 @@ export const sendEmail = async (to, subject, html) => {
   console.log("TO:", to);
   console.log("SUBJECT:", subject);
   console.log("FROM:", process.env.EMAIL);
-  console.log("Sending with Nodemailer...");
-
+  console.log("send from nodemailer");
   try {
-    const info = await Promise.race([
-      transporter.sendMail({
-        from: `"Yumify" <${process.env.EMAIL}>`,
-        to,
-        subject,
-        html,
-      }),
-
-      new Promise((_, reject) =>
-        setTimeout(() => reject(new Error("Email sending timeout")), 15000),
-      ),
-    ]);
+    const info = await transporter.sendMail({
+      from: `"Yumify" <${process.env.EMAIL}>`,
+      to,
+      subject,
+      html,
+    });
 
     console.log("Email sent successfully:", info.messageId);
 
     return info;
   } catch (error) {
-    console.error("EMAIL ERROR:", error);
+    console.error("EMAIL ERROR:", error.message);
     throw error;
   }
 };
--- server-side/utils/tokenGen.util.js
+++ server-side/utils/tokenGen.util.js
@@ -9,8 +9,8 @@ export const generateToken = (res, user) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "12h" });
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: false, // because you're using HTTP
+    sameSite: "lax",
     maxAge: 12 * 60 * 60 * 1000,
   });
 
