93f74fef6ae9cd7ecd3b50cb5c8dd5f1a7cbdf42
Mohamed Essam <mohamedessam9523@gmail.com>
2026-06-29 00:55:04 +0300

    Fix multer upload paths for AWS deployment

--- server-side/middlewares/upload.middleware.js
+++ server-side/middlewares/upload.middleware.js
@@ -1,40 +1,40 @@
+import multer from "multer";
+import path from "path";
 
-import multer from "multer"; // Import Multer for handling file uploads
-import path from "path"; // Import path module for handling file paths
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let uploadPath;
 
-
-
-const storage = multer.diskStorage({ // Configure storage settings for Multer
-  destination: function (req, file, cb) { // cb is callback function to specify destination
-    if(req.baseUrl.includes('/foods')) {
-        cb(null, "uploads/foods"); // Save food images to uploads/foods directory
-    } else if (req.baseUrl.includes('/user')) {
-        cb(null, "uploads/users"); // Save user avatars to uploads/avatars directory
-  }else {
-        cb(new Error('invalid path route')); 
+    if (req.baseUrl.includes("/foods")) {
+      uploadPath = path.join(process.cwd(), "uploads", "foods");
+    } else if (req.baseUrl.includes("/user")) {
+      uploadPath = path.join(process.cwd(), "uploads", "users");
+    } else {
+      return cb(new Error("Invalid path route"));
     }
 
-},
+    cb(null, uploadPath);
+  },
 
   filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname); // Specify the filename for uploaded files
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
-export const upload = multer({ 
-  storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
+export const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+
   fileFilter: (req, file, cb) => {
-    const fileTypes = /jpeg|jpg|png|webp/; // Allowed file types
-    const extname = path.extname(file.originalname).toLowerCase(); // Get file extension
-    // mimetype check is to ensure the file content matches the extension
-    if (fileTypes.test(extname) && fileTypes.test(file.mimetype)) {// Check if file type is allowed
-      cb(null, true); // Accept file
-  }else {
-      cb(new Error("Only images are allowed (jpeg, jpg, png)")); // Reject file
+    const fileTypes = /jpeg|jpg|png|webp/;
+    const extname = path.extname(file.originalname).toLowerCase();
+
+    if (fileTypes.test(extname) && fileTypes.test(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only images are allowed (jpeg, jpg, png, webp)"));
     }
   },
- }); // Create Multer instance with defined storage settings
-
+});
 
 export default upload;
